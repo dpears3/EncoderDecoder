8e997f73c2fea3bdc0a9987de032f2091adc46a1
junkyu1 <71102512+junkyu1@users.noreply.github.com>
2021-04-28 11:33:05 -0400

    Update decoder_k5.sv

--- Decoder/decoder_k5.sv
+++ Decoder/decoder_k5.sv
@@ -58,9 +58,7 @@ module decoder_sys_5(encoded_bits, choose_constraint_length, final_output, clk);
     // How the states are described
     reg [3:0] states [0:7] = {4'b0000, 4'b1000, 4'b0100, 4'b1100, 4'b0010, 4'b1010, 4'b0110, 4'b1110, 4'b0001, 4'b1001, 4'b0101, 4'b1101,4'b0011, 4'b1011, 4'b0111, 4'b1111};
     
-    logic [1:0] given_input_next_output [0:31] = {2'b00, 2'b11, 2'b11, 2'b00, 2'b10, 2'b01, 2'b01, 2'b10, 2'b11, 2'b00, 2'b00, 2'b11, 2'b01, 2'b10, 2'b10, 2'b01, 2'b00, 2'b11, 2'b11, 2'b00, 2'b10, 2'b01, 2'b01, 2'b10, 2'b11, 2'b00, 2'b00, 2'b11, 2'b01, 2'b10, 2'b10, 2'b01};
-                // States: 0 (000) 1 (100) 2 (010) 3 (110) 4 (001) 5 (101) 6 (011) 7 (111)
-                // input/output 0/00, 1/11, 0/11, 1/00, 0/10, 1/01, 0/01, 1/10, 0/11, 1/00, 0/00, 1/11, 0/01, 1/10, 0/10, 1/01
+    logic [1:0] given_input_next_output [0:31] = {2'b00, 2'b11, 2'b10, 2'b01, 2'b10, 2'b01, 2'b00, 2'b11, 2'b10, 2'b01, 2'b11, 2'b00, 2'b11, 2'b00, 2'b01, 2'b10, 2'b11, 2'b00, 2'b01, 2'b10, 2'b01, 2'b10, 2'b11, 2'b00, 2'b10, 2'b01, 2'b00, 2'b11, 2'b00, 2'b11, 2'b10, 2'b01}
     
     // Trellis connecting variable, paired with best_path, Destination: Origin
     reg [3:0] trellis_connection [0:31] = {0, 8, 1, 9, 2, 10, 3, 11, 4, 12, 5, 13, 6, 14, 7, 15};
