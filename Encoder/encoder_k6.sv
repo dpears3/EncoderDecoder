dad2638eaa3cbf45d159da80c9d89e3a84ac9565
dpears3 <60954365+dpears3@users.noreply.github.com>
2021-05-13 22:12:22 -0400

    Update encoder_k6.sv

--- Encoder/encoder_k6.sv
+++ Encoder/encoder_k6.sv
@@ -20,19 +20,19 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module encoder_k6(unencoded_bits, clk, rst, choose_constraint_length, out);
+module encoder_k6(unencoded_bit, clk, rst, choose_constraint_length, out);
 
-    input unencoded_bits, clk, rst;
+    input unencoded_bit, clk, rst;
     input [2:0] choose_constraint_length;
     output reg [1:0] out;
     
     reg [4:0] shift_reg = 0; //register
     
     always @(posedge clk) begin
-        out[1] <= ((unencoded_bits^shift_reg[4])^shift_reg[2])^shift_reg[0];
-        out[0] <= (((unencoded_bits^shift_reg[3])^shift_reg[2])^shift_reg[1])^shift_reg[0];
+        out[1] <= ((unencoded_bit^shift_reg[4])^shift_reg[2])^shift_reg[0];
+        out[0] <= (((unencoded_bit^shift_reg[3])^shift_reg[2])^shift_reg[1])^shift_reg[0];
         shift_reg = shift_reg>>1;
-        shift_reg[4] = unencoded_bits;
+        shift_reg[4] = unencoded_bit;
     end
 
 endmodule
